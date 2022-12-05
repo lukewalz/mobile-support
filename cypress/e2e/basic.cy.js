935085abb6ae43146253035c29a6fd411c7e9eef
Luke Walz <luke.walz@opendorse.com>
2022-12-05 15:59:46 -0600

    form

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,17 +1,13 @@
-describe('sample test', () => {
+describe("sample test", () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit("/");
+  });
 
-  it('displays the resources text', () => {
-    cy.get('h1')
-    .contains('Welcome to my app!');
-  })
-  it('renders the Netlify logo image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
-})
+  it("renders the Netlify logo image", () => {
+    cy.get("img")
+      .should("be.visible")
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+  });
+});

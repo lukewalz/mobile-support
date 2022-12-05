c8fb6eaa3c282f372ff8f1c77256bded80514742
Luke Walz <luke.walz@opendorse.com>
2022-12-05 15:58:13 -0600

    form

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -11,13 +11,42 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="The Driver Omaha Support" />
+        <form
+          name="support"
+          method="POST"
+          data-netlify="true"
+          style={{ width: "100%" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              width: "70%",
+              justifyContent: "center",
+              gap: 10,
+            }}
+          >
+            <label for="email">Email Address:</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              required
+              minlength="10"
+              maxlength="20"
+            />
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+            <label for="question">Question:</label>
+            <textarea type="text" id="question" name="question" required />
+            <input type="hidden" name="support" value="contact" />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
